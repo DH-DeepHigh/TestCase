17c191f593ffcf0ffcce3f2bd2e1ef292be50a16
choihs0457 <choihs0457@gmail.com>
2024-10-17 11:29:16 +0900

    aave setting

--- Aave/test/Borrowing.t.sol.sol
+++ Aave/test/Borrowing.t.sol.sol
@@ -2,6 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import "../src/Mock/MockValue.sol";
 
-contract CounterTest is Test {
+contract BorrowingTest is Test {
+    // address user = State.user_1;
+    // function setUp() public {
+    //     vm.createSelectFork("ETH_RPC_URL", State.BLOCK_NUMBER);
+    // }
+
+    // function test_test() public {
+    //     assertEq(block.number, 20_941_968, "fail");
+    // }
+
 }
--- Aave/test/CollateralSupply.t.sol
+++ Aave/test/CollateralSupply.t.sol
@@ -1,7 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
+import "../src/interfaces/IPool.sol";
+import "../src/interfaces/IPoolAddressesProvider.sol";
+import "../src/Mock/MockValue.sol";
+import "../src/Mock/MockAddress.sol";
+import {IERC20} from '../src/dependencies/openzeppelin/contracts/IERC20.sol';
+import "../src/protocol/libraries/types/DataTypes.sol";
 
 contract CounterTest is Test {
+    IPool Pool = IPool(AddressList.POOL);
+    IERC20 WBTC = IERC20(AddressList.WBTC);
+    address user = TestState.user_1;
+    function setUp() public {
+        vm.createSelectFork("ETH_RPC_URL", TestState.BLOCK_NUMBER);
+    }
+
+    function test_simple_supply() public {
+        vm.startPrank(user);
+        deal(address(WBTC), user, 100 ether);
+
+        WBTC.approve(address(Pool), 100 ether);
+        Pool.supply(address(WBTC), 1 * 1e8, user, 0);
+        // DataTypes.ReserveData memory ReserveData = Pool.getReserveData(address(WBTC));
+        // DataTypes.ReserveData memory ReserveData = Pool.getReserveData(address(user));
+        DataTypes.UserConfigurationMap memory UserConfiguration = Pool.getUserConfiguration(address(user));
+        // (uint256 totalCollateralBase,
+        // uint256 totalDebtBase,
+        // uint256 availableBorrowsBase,
+        // uint256 currentLiquidationThreshold,
+        // uint256 ltv,
+        // uint256 healthFactor) = Pool.getUserAccountData(user);
+        // uint256 RewardAWBTC = IERC20(address(ReserveData.aTokenAddress)).balanceOf(user);
+        // IERC20 ExpectedLiquidityAWBTC = IERC20(address(ReserveData.aTokenAddress));
+
+        // require(RewardAWBTC == ExpectedLiquidityAWBTC, "");
+
+
+    }
 }
