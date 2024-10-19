77a7424b1fdb279f7c49226ea94c3ce330e67e2e
upside <castle@upsideui-MacBookPro.local>
2024-10-19 15:00:27 +0900

    ADD: Compound TestCase

--- Compound/test/ProtocolManagement.t.sol
+++ Compound/test/ProtocolManagement.t.sol
@@ -7,11 +7,16 @@ import "../src/TestFile.sol";
 /// @notice Example contract that calculates the account liquidity.
 contract admin_ComptrollerTest is Test, TestUtils {
     address payable user =payable(makeAddr('user'));
+    address pauseGuardian;
+    address borrowCapGuardian;
+
     function setUp() public {
         // Fork mainnet at block 20_941_968.
         cheat.createSelectFork("mainnet", BLOCK_NUMBER);
+        pauseGuardian = comptroller.pauseGuardian();
+        borrowCapGuardian = comptroller.borrowCapGuardian();
     }
-        function test_setPriceOracle() public {
+    function test_setPriceOracle() public {
         vm.startPrank(admin);
         comptroller._setPriceOracle(address(0x1));
         assertEq(comptroller.oracle(), address(0x1));
@@ -372,4 +377,78 @@ contract admin_ComptrollerTest is Test, TestUtils {
         assertEq(unitroller.admin(),user);
         vm.stopPrank();
     }
+    function test_setMintPaused_guardian() public {
+        vm.startPrank(pauseGuardian);
+        comptroller._setMintPaused(cEther, true);
+        bool state = comptroller.mintGuardianPaused(address(cEther));
+        assertEq(state,true);
+        
+        vm.expectRevert("only admin can unpause");
+        comptroller._setMintPaused(cEther, false);
+        vm.stopPrank();
+
+        vm.expectRevert("only pause guardian and admin can pause");
+        comptroller._setMintPaused(cEther, true);
+    }
+    function test_setBorrowPaused_guardian() public {
+        vm.startPrank(pauseGuardian);
+        comptroller._setBorrowPaused(cEther, true);
+        bool state = comptroller.borrowGuardianPaused(address(cEther));
+        assertEq(state,true);
+        
+        vm.expectRevert("only admin can unpause");
+        comptroller._setBorrowPaused(cEther, false);
+        vm.stopPrank();
+        
+        vm.expectRevert("only pause guardian and admin can pause");
+        comptroller._setBorrowPaused(cEther, true);
+    }
+    function test_setTransferPaused_guardian() public {
+        vm.startPrank(pauseGuardian);
+        comptroller._setTransferPaused(true);
+        bool state = comptroller.transferGuardianPaused();
+        assertEq(state,true);
+        
+        vm.expectRevert("only admin can unpause");
+        comptroller._setTransferPaused(false);
+        vm.stopPrank();
+        
+        vm.expectRevert("only pause guardian and admin can pause");
+        comptroller._setTransferPaused(true);
+    }
+    function test_setSeizePaused_guardian() public {
+        vm.startPrank(pauseGuardian);
+        comptroller._setSeizePaused(true);
+        bool state = comptroller.seizeGuardianPaused();
+        assertEq(state,true);
+        
+        vm.expectRevert("only admin can unpause");
+        comptroller._setSeizePaused(false);
+        vm.stopPrank();
+        
+        vm.expectRevert("only pause guardian and admin can pause");
+        comptroller._setSeizePaused(true);
+    }
+    function test_setMarketBorrowCaps_guardian() public {
+        CTokenInterface[] memory cTokens =new CTokenInterface[](1);
+        cTokens[0]=cEther;
+        uint[] memory newBorrowCaps = new uint[](1);
+        newBorrowCaps[0]=1e18;
+
+        uint[] memory wrongBorrowCaps = new uint[](2);
+        wrongBorrowCaps[0]=1e18;
+        wrongBorrowCaps[1]=2e18;
+        
+        vm.startPrank(borrowCapGuardian);
+        comptroller._setMarketBorrowCaps(cTokens, newBorrowCaps);
+
+        vm.expectRevert("invalid input");
+        comptroller._setMarketBorrowCaps(cTokens, wrongBorrowCaps);
+        
+        vm.stopPrank();
+
+        vm.expectRevert("only admin or borrow cap guardian can set borrow caps");
+        comptroller._setMarketBorrowCaps(cTokens, newBorrowCaps);
+    }
+
 }
